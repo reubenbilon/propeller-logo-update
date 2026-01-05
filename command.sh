30b040f743a730adaab0bd9cddb9087d6def4260
reubenbilon <119168867+reubenbilon@users.noreply.github.com>
2026-01-05 13:00:32 +0530

    Add command.sh for logo update process documentation

---
+++ command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# REPO_URL https://github.com/reubenbilon/propeller-logo-update
+
+# Repository created manually on GitHub (public)
+
+# Created update_logo branch from master manually
+
+# Replaced src/logo.svg with animated Vector.svg (clearly visible spinning)
+
+# Updated existing link to:
+# https://www.propelleraero.com/dirtmate/
+
+# Created Pull Request (update_logo -> master)
+# Merged Pull Request manually via GitHub UI
